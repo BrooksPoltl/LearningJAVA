816290efb98508c5d12467ad82b0721c108b925c
BrooksPoltl <bpoltl1@gmail.com>
2019-08-01 16:38:20 -0500

    byte short int

---
+++ byteshortint/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int myMinValue = -2_147_483_648;        
+        int myMaxValue = 2_147_483_647;
+        System.out.println(myMinValue);
+        System.out.println(myMaxValue);
+
+        byte myByteValue = -128;
+        short myShortValue = 32767;
+
+        System.out.println(myByteValue);
+        System.out.println(myShortValue);
+    
+    
+    }
+}
