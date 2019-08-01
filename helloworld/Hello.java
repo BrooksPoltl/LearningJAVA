b59b109fb977a9caf16638061a03aa46294a3c91
BrooksPoltl <bpoltl1@gmail.com>
2019-08-01 16:26:26 -0500

    printing variables

--- helloworld/Hello.java
+++ helloworld/Hello.java
@@ -1,5 +1,8 @@
 class Hello{
     public static void main(String[] args ) {
         System.out.println("Hello world");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
     }
 }
