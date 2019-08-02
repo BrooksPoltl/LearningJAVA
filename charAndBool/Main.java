05cd9b65593c491fd2012d7fd808e6a530d3d1b1
BrooksPoltl <bpoltl1@gmail.com>
2019-08-02 09:40:42 -0500

    char and bool

---
+++ charAndBool/Main.java
@@ -0,0 +1,11 @@
+class Main{
+
+    public static void main(String[] args) {
+        char myChar = '\u00A9';
+
+        System.out.println("Unicode output was: " + myChar);
+
+        boolean myBoolean = true;
+        
+    }
+}
