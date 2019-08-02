ad93cdbfff0a2e92dd11804152ee9e7ba02b145b
BrooksPoltl <bpoltl1@gmail.com>
2019-08-02 11:13:35 -0500

    scanner

---
+++ readingUserInput/Main.java
@@ -0,0 +1,15 @@
+
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your name: ");
+        String name = scanner.nextLine();
+        
+        System.out.println("Your name is " + name);
+
+        scanner.close();
+    }
+}
