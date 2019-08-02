02809caba9de6ebc385ba2fdb4217b1b32d9ad10
BrooksPoltl <bpoltl1@gmail.com>
2019-08-02 09:34:00 -0500

    completed challenge

---
+++ floatChallenge/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        int pounds = 100;
+        double kilograms = pounds/.45359237d;
+        
+        System.out.println(kilograms);
+    }
+}
