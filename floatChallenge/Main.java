077d88b6e0dc606ec96d05778c73614b078c8a7a
BrooksPoltl <bpoltl1@gmail.com>
2019-08-02 09:35:22 -0500

    whoops

--- floatChallenge/Main.java
+++ floatChallenge/Main.java
@@ -1,8 +1,9 @@
 public class Main{
     public static void main(String[] args) {
-        int pounds = 100;
-        double kilograms = pounds/.45359237d;
-        
+        int pounds = 200;
+        double kilograms = pounds*.45359237d;
+
         System.out.println(kilograms);
     }
 }
+
